c271b0e5edbfde38cd504a7bfbdd0b232ff4a74b
Mohammad Jhare <jhm1yok@bosch.com>
2025-01-08 18:44:08 +0000

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+RESET='\033[0m'
+
+# Install python virtual environment
+echo -e "${CYAN}Installing Python virtual environment.${RESET}"
+python -m venv env
+
+# Active the virtual environment
+echo -e "${CYAN}Activating virtual environment.${RESET}"
+activate_env () { . $PWD/env/Scripts/activate; }
+activate_env
+
+# Upgrade pip
+echo -e "${CYAN}Upgrading pip.${RESET}"
+python -m pip install -U pip
+
+# Install python requirements 
+echo -e "${CYAN}Installing requirements.${RESET}"
+pip install -r ./requirements.txt
+
+echo -e "${GREEN}Done.${RESET}"
+
